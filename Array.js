28b94640756c8fa650ad499171c09842056f0104
Faiazur Rahman <faiazurrahman748@gmail.com>
2025-05-14 23:15:50 +0600

    Array

---
+++ Array.js
@@ -0,0 +1,13 @@
+// Creating and Printing an array
+const array = [1,2,3,4,5]
+console.log(array)
+
+let fruits=["Apple","Banana","Mango","Grapes","Strawberry"]
+console.log(fruits);
+
+
+//Array.push, pushes element to the end of array and unshift pushes elemt to the start of the array
+const numbah=[1,2,3,4,5];
+numbah.push(5);
+numbah.unshift("Array Startoo - ");
+console.log(numbah);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ProjectCodeFundamentalsDay05
+
+Arrays
